35af9d7042167a7295537f3d21717e5220f32636
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2023-03-17 20:25:28 +0530

    updated animation final

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,21 +1,21 @@
 import React from 'react'
-// import Lottie from 'react-lottie'
-// import animationData from '../assets/animation/final.json'
+import Lottie from 'react-lottie'
+import animationData from '../assets/animation/tanujnarang221.json'
 // import animationData2 from '../assets/animation/final_1.lottie'
-import animationData3 from '../assets/animation/final_2.webm'
-import '@dotlottie/player-component';
+// import animationData3 from '../assets/animation/final_2.webm'
+// import '@dotlottie/player-component';
 
 const Animation = () => {
+  const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    };
   return (
-    <div>
-      <dotlottie-player
-        src={JSON.stringify(animationData3)}
-        autoplay
-        loop
-        style={{ height: '100%', width: '100%' }}
-      />
-      {/* <Lottie options={defaultOptions} isClickToPauseDisabled={true} height={650} width={'auto'} /> */}
-    </div>
+      <Lottie options={defaultOptions} isClickToPauseDisabled={true} height={600} width={'auto'} />
   )
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ThreeUser from '../assets/icons/3User.svg';
-import AddUser from '../assets/icons/addUser.svg';
-import Document from '../assets/icons/Document.svg';
-import TimeCircle from '../assets/icons/timeCircle.svg';
-// import Animation from './Animation';
-import Animation2 from '../assets/animation/final_2.webm';
+import Animation from './Animation';
+// import Animation2 from '../assets/animation/final_2.webm';
 
 const Hero = () => {
   return (
@@ -12,34 +8,33 @@ const Hero = () => {
     <div className='wrapper bg-[#FFFFFF] flex items-start flex-col justify-between pl-[3rem] lg:pl-[11rem] w-[100%] md:min-h-[100%] relative'>
       {/* Left side */}
       <div className='headings flex flex-col items-start justify-center lg:h-[100%] font-bold leading-none py-[1rem] lg:pt-[4rem] text-[1rem] md:text-[2rem] lg:text-[2.6rem] xl:text-[3.4rem] 2xl:text-[4rem]'>
-        <span>Hire Software</span>
-        <span>Engineers of</span>
-        <span className='text-[#2A85FF]'>Top Development</span>
+        <p><span className='text-[#2A85FF]'>Hyrefast </span>Engineers</p>
+        <span>from Top Global</span>
+        <span>Development</span>
         <span>Companies</span>
-        <div className='heroCard w-[300px] h-[87px] mt-[2rem] bg-[#F4F4F4] text-[#6F767E] text-[.9rem] font-normal rounded-2xl leading-4 p-[.4rem]'>
-          <ul className='flex flex-wrap'>
-            <li className='wrapper flex items-center px-[.35rem] py-[.5rem]'>
-              <img src={Document} alt='logo' className='' />
-              <p className='ml-[.5rem]'>Flexible Contracts</p>
+        <div className='text-[#000] text-base font-normal rounded-2xl leading-4 py-4'>
+          <ul>
+            <li className='wrapper flex items-center px-[.35rem] py-1'>
+              <p>-> Pre-hired Engineers: Zero Drop Outs</p>
             </li>
-            <li className='wrapper flex px-[.35rem] py-[.5rem]'>
-              <img src={AddUser} alt='logo' className='' />
-              <p className='ml-[.5rem]'>No freelancers</p>
+            <li className='wrapper flex px-[.35rem] py-1'>
+              <p>-> Deployment: 48 hrs. from selections</p>
             </li>
-            <li className='wrapper flex px-[.35rem] pt-[.7rem]'>
-              <img src={ThreeUser} alt='logo' className='' />
-              <p className='ml-[.5rem]'>Team in 5 days</p>
+            <li className='wrapper flex px-[.35rem] py-1'>
+              <p>-> Flexible Contract</p>
             </li>
-            <li className='wrapper flex px-[.35rem] pt-[.7rem] ml-[1.4rem]'>
-              <img src={TimeCircle} alt='logo' className='' />
-              <p className='ml-[.5rem]'>Time Zone</p>
+            <li className='wrapper flex px-[.35rem] py-1'>
+              <p>-> Across Timezones</p>
+            </li>
+            <li className='wrapper flex px-[.35rem] py-1'>
+              <p>-> No Freelancers: Easy Reliability</p>
             </li>
           </ul>
         </div>
       </div>
-      <div className='lg:absolute relative sm:w-[100%] md:w-[60%] lg:[65%] md:z-50 md:top-0 md:right-0'>
-        {/* <Animation /> */}
-        <video src={Animation2} playsInline autoPlay loop muted type='video/webm' />
+      <div className='lg:absolute relative sm:w-[100%] md:w-[55%] lg:[60%] md:z-50 md:top-0 md:right-0'>
+        <Animation />
+        {/* <video src={Animation2} playsInline autoPlay loop muted type='video/webm' /> */}
       </div>
     </div>
     <img src={require('../assets/images/floor.jpg')}
