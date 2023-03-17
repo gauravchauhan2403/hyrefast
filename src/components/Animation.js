9eda6a77c1d9b80234cc7602fe824342b898e702
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2023-03-17 21:39:16 +0530

    updated animation final

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Lottie from 'react-lottie'
-import animationData from '../assets/animation/tanujnarang221.json'
+import animationData from '../assets/animation/tanujnarang1.json'
 // import animationData2 from '../assets/animation/final_1.lottie'
 // import animationData3 from '../assets/animation/final_2.webm'
 // import '@dotlottie/player-component';
