b7127977726caa0f0076e40b59dd324632f1d498
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2023-03-19 01:32:31 +0530

    update

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -15,7 +15,7 @@ const Animation = () => {
       }
     };
   return (
-      <Lottie options={defaultOptions} isClickToPauseDisabled={true} height={700} width={'auto'} />
+      <Lottie options={defaultOptions} isClickToPauseDisabled={true} height={650} width={'auto'} />
   )
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import Animation from './Animation';
+import ArrowRight from '../assets/icons/arrow-right.svg';
 // import Animation2 from '../assets/animation/final_2.webm';
 
 const Hero = () => {
   return (
     <div>
-    <div className='wrapper bg-[#FFFFFF] flex items-start flex-col pl-[3rem] lg:pl-[11rem] w-[100%] md:min-h-[100%] relative'>
+    <div className='wrapper bg-[#FFFFFF] flex items-start justify-between flex-col pl-[2rem] lg:pl-[10rem] w-[100%] md:min-h-[100%] relative'>
       {/* Left side */}
-      <div className='headings flex flex-col items-start justify-center lg:h-[100%] font-bold leading-none ml-10 lg:pt-[4rem] text-[1rem] md:text-[2rem] lg:text-[2.6rem] xl:text-[3.4rem] 2xl:text-[4rem]'>
+      <div className='headings flex flex-col items-start justify-center lg:h-[100%] font-bold leading-none pt-2 lg:pt-[4rem] text-[2rem] md:text-[2rem] lg:text-[3.2rem]'>
         <p><span className='text-[#2A85FF]'>Hyrefast </span>Engineers</p>
         <span>from Top Global</span>
         <span>Development</span>
         <span>Companies</span>
-        <div className='text-[#000] text-base font-normal rounded-2xl leading-4 py-4'>
+        <div className='text-[#000] mt-6 text-base font-normal rounded-2xl leading-4 py-4'>
           <ul>
             <li className='wrapper flex items-center px-[.35rem] py-1'>
-              <p>-> Pre-hired Engineers: Zero Drop Outs</p>
+              <img src={ArrowRight} alt='' className='mx-1 h-4 w-4' />
+              <p>Pre-hired Engineers: Zero Drop Outs</p>
             </li>
             <li className='wrapper flex px-[.35rem] py-1'>
-              <p>-> Deployment: 48 hrs. from selections</p>
+              <img src={ArrowRight} alt='' className='mx-1 h-4 w-4' />
+              <p>Deployment: 48 hrs. from selections</p>
             </li>
             <li className='wrapper flex px-[.35rem] py-1'>
-              <p>-> Flexible Contract</p>
+              <img src={ArrowRight} alt='' className='mx-1 h-4 w-4' />
+              <p>Flexible Contract</p>
             </li>
             <li className='wrapper flex px-[.35rem] py-1'>
-              <p>-> Across Timezones</p>
+              <img src={ArrowRight} alt='' className='mx-1 h-4 w-4' />
+              <p>Across Timezones</p>
             </li>
             <li className='wrapper flex px-[.35rem] py-1'>
-              <p>-> No Freelancers: Easy Reliability</p>
+              <img src={ArrowRight} alt='' className='mx-1 h-4 w-4' />
+              <p>No Freelancers: Easy Reliability</p>
             </li>
           </ul>
         </div>
